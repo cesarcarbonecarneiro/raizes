a47a756eff029e8bb18e9828c6d856290a01d012
Gabriel Casagrande <gabrielcasagrande.zetta@ufla.br>
2021-02-18 00:06:52 -0300

    quarta de cinzas

---
+++ raizes-front/src/components/Homepage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="Homepage">
+    <Navbar /> 
+  </div>
+</template>
+
+<script>
+import Navbar from './Navbar'
+
+export default {
+  name: 'Homepage',
+  props: {
+    msg: String
+  },
+  components : {
+    Navbar
+  }
+}
+</script>
+
+<style scoped>
+.logo-container {
+  display: inline;
+}
+</style>
---
+++ raizes-front/src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="Navbar">
+    <img src="../assets/logo.png" height="30px">
+    <div class="title">
+      <h1>RAÍZES</h1>
+      <h3>restaurante</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<style>
+.Navbar {
+  display: inline-block;
+}
+
+.title {
+  display: flex;
+  align-items: baseline;
+}
+
+</style>
